1ba3e7bfc8c1609604f390b751b1aed7512c4da9
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-06-28 18:38:30 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+const isProd = import.meta.env.MODE === 'production';
+const basename = isProd ? '/TimeScape_II' : '/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/TimeScape_II">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
