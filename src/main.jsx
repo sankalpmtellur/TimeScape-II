b007cc5e863d0f5ab9170adc8d1d3b6a93ef861e
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-06-28 17:23:20 +0530

    Updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
+// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/TimeScape-II">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
